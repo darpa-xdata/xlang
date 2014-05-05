8d774e3083cb231048c1618da525813514103faf
ericwhyne <ericwhyne@gmail.com>
2014-05-05 10:41:07 -0400

    Update examples.sh

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 echo "test for" > /echo.txt
+# good practics to do this before installing software
 sudo apt-get update
-# Hadoop requires a Java runtime enviornment be available
+
+# -y will install packages without human interaction
 sudo apt-get -y install  openjdk-6-jdk
-# Necessary for automatically creating the hdfs user 
 sudo apt-get -y install makepasswd
 
-# Create the hdfs user
-PASSWORD=anexamplepasswordidontmindbeingongithub
+# An example of how to create a user
+#PASSWORD=never_put_passwords_on_github
+PASSWORD=`cat ~/.ssh/a_protected_file_with_a_secure_password_in_it`
 export SHELL=/bin/bash
 echo $PASSWORD | sudo tee -a /tmp/password.txt
 passhash=$(makepasswd --clearfrom=/tmp/password.txt --crypt-md5 |awk '{print $2}')
