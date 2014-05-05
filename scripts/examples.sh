6408161d3411c9b6da8c91ac88962da7e7b6d514
ericwhyne <ericwhyne@gmail.com>
2014-05-05 10:42:58 -0400

    Update examples.sh

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 echo "test for" > /echo.txt
-# good practics to do this before installing software
+# do this before installing software
 sudo apt-get update
 
 # -y will install packages without human interaction
 sudo apt-get -y install  openjdk-6-jdk
 sudo apt-get -y install makepasswd
 
-# An example of how to create a user
+# an example of how to create a user semi-securely
 #PASSWORD=never_put_passwords_on_github
 PASSWORD=`cat ~/.ssh/a_protected_file_with_a_secure_password_in_it`
 export SHELL=/bin/bash
@@ -17,7 +17,7 @@ passhash=$(makepasswd --clearfrom=/tmp/password.txt --crypt-md5 |awk '{print $2}
 sudo useradd hdfs -p $passhash
 #sudo rm /tmp/password.txt
 
-# copy hadoop files to the hdfs user home dir
+# examples of how to wget and copy files around
 pwd
 sudo cp -r /vagrant/files/xdata_cdh4.2 /home/hdfs/
 wget http://archive.cloudera.com/cdh4/cdh/4/hadoop-2.0.0-cdh4.2.2.tar.gz -P /home/hdfs/
@@ -25,7 +25,7 @@ sudo tar -xzf /home/hdfs/hadoop-2.0.0-cdh4.2.2.tar.gz -C /home/hdfs/
 sudo ln -s /home/hdfs/hadoop-2.0.0-cdh4.2.2 /home/hdfs/hadoop
 sudo chown -R hdfs:hdfs /home/hdfs/*
 
-# Environment variables necessary to run hadoop
+# how to set environment variables and make them permanent
 export HADOOP_HOME=/home/hdfs/hadoop
 echo HADOOP_HOME=/home/hdfs/hadoop | sudo tee -a /etc/environment
 export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
