fdbc12e417210041f6abed3d7116131ceb456f57
Andy R. Terrel <andy.terrel@gmail.com>
2014-05-07 22:40:51 -0500

    Fixing more Python details

--- thunderdome/td_python.c
+++ thunderdome/td_python.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <limits.h>
 #include <string.h>
 #include <assert.h>
 
@@ -9,71 +11,119 @@
 
 // value mapping
 
-static void to_td_val(td_val_t *out, PyObject *pVal)
+static td_tag_t longlong_to_td(long long v)
 {
+	if (INT32_MIN <= v <= INT32_MAX) return TD_INT32;
+	else if (0 <= v <= UINT32_MAX) return TD_UINT32;
+	else return TD_INT64;
+}
 
+static td_tag_t py_type_to_td(PyObject *pVal)
+{
     if (PyInt_Check(pVal)){
-        out->tag = TD_INT32;
-        out->int32_val = PyInt_AsLong(pVal);
-        return;
+#if INT32_MAX==LONG_MAX
+        return TD_INT32;
+#else
+        return longlong_to_td(PyInt_AS_LONG(pVal));
+#endif
     }
     else if (PyLong_Check(pVal)){
         PyObject *pErr;
-        long v;
-        v = PyLong_AsLong(pVal);
-        if ( (pErr = PyErr_Occurred()) && PyErr_ExceptionMatches(PyExc_OverflowError)) {
+        long long v;
+        v = PyLong_AsLongLong(pVal);
+        if ( v == -1 && PyErr_Occurred()) {
+            // could not eval as long long try unsigned long long later
             PyErr_Clear();
         }
         else {
-            out->tag = TD_INT32;
-            out->int32_val = v;
-            return;
+        	return longlong_to_td(PyInt_AS_LONG(v));
         }
 
-        unsigned long vv;
-        vv = PyLong_AsUnsignedLong(pVal);
-        if ( (pErr = PyErr_Occurred()) && PyErr_ExceptionMatches(PyExc_OverflowError)){
+        unsigned long long vv;
+        vv = PyLong_AsUnsignedLongLong(pVal);
+        if ( v == (unsigned long long) -1 && PyErr_Occurred() ) {
             PyErr_Clear();
+            return TD_OBJECT;
         }
         else {
-            out->tag = TD_UINT32;
-            out->uint32_val = vv;
-            return;
+            return TD_UINT64;
         }
-
-        long long vvv;
-        vvv = PyLong_AsLongLong(pVal);
-        if ( (pErr = PyErr_Occurred()) && PyErr_ExceptionMatches(PyExc_OverflowError)) {
-            PyErr_Clear();
-        }
-        else {
-            out->tag = TD_INT64;
-            out->int64_val = vvv;
-            return;
-        }
-
-        unsigned long long vvvv;
-        vvvv = PyLong_AsUnsignedLongLong(pVal);
-        if ( (pErr = PyErr_Occurred()) && PyErr_ExceptionMatches(PyExc_OverflowError)) {
-            PyErr_Clear();
-        }
-        else {
-            out->tag = TD_UINT64;
-            out->int64_val = vvvv;
-            return;
-        }
-
-        // Didn't match any pylong, revert to obj.
     }
-    else if(PyFloat_Check(pVal)) {
+    else if (PyFloat_Check(pVal)) {
+        return TD_DOUBLE;
+    }
+    else if (PyUnicode_Check(pVal)){
+        PyObject *pStr;
+        pStr = PyUnicode_AsUTF8String(pVal);
+        if ( pStr && PyErr_Occurred() ){
+            PyErr_Clear();
+            return TD_OBJECT;
+        }
+        else {
+            return TD_UTF8;
+        }
+    }
+    else if (PyString_Check(pVal)) {
+        return TD_UTF8;
+    }
+    return TD_OBJECT;
+}
+
+
+
+static void to_td_val(td_val_t *out, PyObject *pVal)
+{
+    PyObject *pStr;
+    td_tag_t tag = py_type_to_td(pVal);
+    switch (tag) {
+    case TD_INT32:
+        out->tag = TD_INT32;
+        if (PyInt_Check(pVal))
+            out->int32_val = PyInt_AS_LONG(pVal);
+        else
+            out->int32_val = PyLong_AsLong(pVal);
+        break;
+    case TD_UINT32:
+        out->tag = TD_UINT32;
+        out->uint32_val = PyLong_AsUnsignedLong(pVal);
+        break;
+    case TD_INT64:
+        out->tag = TD_INT64;
+        out->int64_val = PyLong_AsLongLong(pVal);
+        break;
+    case TD_UINT64:
+        out->tag = TD_UINT64;
+        out->uint64_val = PyLong_AsUnsignedLongLong(pVal);
+        break;
+    case TD_DOUBLE:
         out->tag = TD_DOUBLE;
         out->double_val = PyFloat_AsDouble(pVal);
-        return;
+        break;
+    case TD_UTF8:
+        pStr = pVal;
+        if (PyUnicode_Check(pStr)) pStr = PyUnicode_AsUTF8String(pStr);
+        size_t len = PyString_Size(pStr);
+        char* data = malloc((len+1)*sizeof(char));
+        strcpy(PyString_AsString(pStr), data);
+        td_string_t *obj = malloc(sizeof(td_string_t));
+        obj->length = len;
+        obj->data = (void*) data;
+        out->tag = TD_UTF8;
+        out->object = (void*) obj;
+        break;
+//    case TD_ARRAY:
+//        return
+//
+//        return (PyObject*)
+//
+//
+//            py_ptr_to_array_1d((PyObject*)py_apply_array_type((py_datatype_t*)td_type_to_jl(td_eltype(v)), 1),
+//                               td_dataptr(v), td_length(v), 0);
+    default:
+        out->tag = TD_OBJECT;
+        out->owner = td_env_python(NULL, NULL);
+        out->object = pVal;
     }
-
-    // Default, only get here if unable to convert above.
-    out->owner = td_env_python(NULL, NULL);
-    out->object = pVal;
 }
 
 static PyObject *from_td_val(td_val_t *v)
@@ -112,7 +162,8 @@ static PyObject *from_td_val(td_val_t *v)
 //            py_ptr_to_array_1d((PyObject*)py_apply_array_type((py_datatype_t*)td_type_to_jl(td_eltype(v)), 1),
 //                               td_dataptr(v), td_length(v), 0);
     default:
-        return v;
+        td_error("Cannot convert td type to Python.\n");
+        return pVal;
     }
 }
 
@@ -219,7 +270,7 @@ void td_py_eval(td_val_t *out, char *str)
 
 td_tag_t td_py_get_type(void *v)
 {
-//    return py_type_to_td(py_typeof((PyObject*)v));
+    return py_type_to_td((PyObject*) v);
 }
 
 td_tag_t td_py_get_eltype(void *v)
@@ -290,3 +341,4 @@ void td_py_init(char *homedir)
     td_provide_python(env);
     printf("Here td_py_init 60\n");
 }
+
