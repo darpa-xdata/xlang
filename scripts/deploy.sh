f4af156db07f4fcea0af57d5bcc024c9505d1711
Wade Shen <swadey@mit.edu>
2014-05-12 21:21:49 -0400

    - add deploy settings for julia

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,13 +7,13 @@ echo "test for" > /echo.txt # 2112 test, if this file exists on the VM then this
 sudo apt-get update
 
 # Install a Java runtime enviornment
-sudo apt-get -y install  openjdk-6-jdk
+sudo apt-get -y install openjdk-6-jdk
 export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
 echo JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64 | sudo tee -a /etc/environment
 
-# Ubuntu Vagrant bug workaround (mesg n hangs in vagrant when no tty exists)
-sudo cat /root/.profile | sudo sed 's/^.*mesg n//' | sudo tee /root/.profile
 
 # Add your deployment scripts here:
 sudo $scriptdir/helloworld.sh
 sudo $scriptdir/R.sh
+sudo $scriptdir/julia.sh
+
