c967166c1cae1d6fd42f9ad442665ff41f7f7fcc
Andy R. Terrel <andy.terrel@gmail.com>
2014-07-26 08:13:52 -0400

    csr to ij test

--- webgraph/cluster/gunrock/gunrock_clusters.h
+++ webgraph/cluster/gunrock/gunrock_clusters.h
@@ -6,7 +6,9 @@
 #include <gunrock.h>
 #include <thunderdome/td.h>
 
-int _td_to_gunrock(graph_t* td_graph, struct GunrockGraph* gr_graph);
 int gunrock_topk(int top_nodes, graph_t* input_td_graph, graph_t* output_graph);
+
+int td_to_gunrock(graph_t* td_graph, struct GunrockGraph* gr_graph);
+
 
 #endif // GUNROCK_CLUSTERS_H
--- webgraph/cluster/gunrock/test_gunrock.c
+++ webgraph/cluster/gunrock/test_gunrock.c
@@ -1,6 +1,14 @@
 #include "gunrock_clusters.h"
 #include <string.h>
 
+// Forward declaration for testing.
+int _ij_to_csc(int num_nodes, int num_edges, int* ij_mat, 
+	       int* csc_col_offsets, int* csc_row_indices);
+int _csr_to_ij(int num_nodes, int num_edges, 
+	       int* csr_row_offsets, int *csr_col_indicies, 
+	       int* ij_mat);
+
+
 int _create_simple_td_graph(graph_t *graph)
 {
   size_t num_nodes = 7;
@@ -41,6 +49,38 @@ int _print_small_array(int* arr, int len){
   return 0;
 }
 
+int test_csr_to_ij()
+{
+  graph_t td_graph;
+
+  printf("Test CSR to IJ\n");
+  int num_nodes = 7;
+  int num_edges = 15;
+
+  unsigned int row_offsets[8] = {0,3,6,9,11,14,15,15};
+  int col_indices[15] = {1,2,3,0,2,4,3,4,5,5,6,2,5,6,6};
+  int ij_mat[30];
+  
+  printf("---> row_offsets\n");
+  printf("-------> ");
+  _print_small_array((int*)row_offsets, num_nodes+1);
+  printf("\n");
+
+  printf("---> col_indices\n");
+  printf("-------> ");
+  _print_small_array((int*)col_indices, num_edges);
+  printf("\n");
+
+  _csr_to_ij(num_nodes, num_edges, row_offsets, col_indices, ij_mat);
+  printf("---> ij_mat\n");
+  printf("------> ");
+  _print_small_array(ij_mat, 30);
+  printf("\n");
+
+  return 0;
+}
+
+
 int test_gunrock_graph_convert()
 {
   graph_t td_graph;
@@ -49,7 +89,7 @@ int test_gunrock_graph_convert()
   printf("Test Gunrock Graph Convert\n");
 
   _create_simple_td_graph(&td_graph);
-  _td_to_gunrock(&td_graph, &gr_graph);
+  td_to_gunrock(&td_graph, &gr_graph);
 
   printf("---> gunrock row_offsets\n");
   printf("-----------> ");
@@ -94,6 +134,7 @@ int test_gunrock_topk()
 int main(int argc, char** argv)
 {
   int ret = 0;
+  ret |= test_csr_to_ij();
   ret |= test_gunrock_graph_convert();
   ret |= test_gunrock_topk();
   return ret;
